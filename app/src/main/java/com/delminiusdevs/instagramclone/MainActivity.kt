916b63ecf726bcac3cc645685a86009659e40bdb
Ivan Milic <milic.tg@gmail.com>
2022-05-20 17:33:11 +0200

    No will for tuts

--- app/src/main/java/com/delminiusdevs/instagramclone/MainActivity.kt
+++ app/src/main/java/com/delminiusdevs/instagramclone/MainActivity.kt
@@ -6,12 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.delminiusdevs.instagramclone.view_model.IgViewModel
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.delminiusdevs.instagramclone.ui.theme.InstagramCloneTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    InstagramApp()
                 }
             }
         }
@@ -30,14 +32,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun InstagramApp(){
+    val vm = hiltViewModel<IgViewModel>()
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    InstagramCloneTheme {
-        Greeting("Android")
-    }
 }
---
+++ app/src/main/java/com/delminiusdevs/instagramclone/auth/SignUpScreen.kt
@@ -0,0 +1,14 @@
+package com.delminiusdevs.instagramclone.auth
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.delminiusdevs.instagramclone.view_model.IgViewModel
+
+@Composable
+fun SignUpScreen(
+    navController: NavController,
+    vm: IgViewModel
+){
+    Text(text = "SignUpScreen")
+}
